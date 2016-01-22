c21673688f05972333dc75614bd9270155648b08
Christos Paraskeva <cparaskeva@ubitech.eu>
2016-01-22 18:12:53 +0200

    Minor fix

--- cloudteams-app/src/main/java/eu/cloudteams/authentication/AuthUtils.java
+++ cloudteams-app/src/main/java/eu/cloudteams/authentication/AuthUtils.java
@@ -8,6 +8,7 @@ import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -17,11 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 public class AuthUtils {
 
     private static final JWSHeader JWT_HEADER = new JWSHeader(JWSAlgorithm.HS256);
-
-    @Value("${token.secret}")
-    private static String TOKEN_SECRET;
-    
-//    private static final String TOKEN_SECRET = "thecakeisalie";
+    private static final String TOKEN_SECRET = "thecakeisalie";
     public static final String AUTH_HEADER_KEY = "Authorization";
 
     /**
@@ -31,13 +28,13 @@ public class AuthUtils {
      * @return A Token object
      * @throws JOSEException
      */
+    @Autowired
     public static Token createToken(String host, long sub) throws JOSEException {
         JWTClaimsSet claim = new JWTClaimsSet();
         claim.setSubject(Long.toString(sub));
         claim.setIssuer(host);
         claim.setIssueTime(DateTime.now().toDate());
         claim.setExpirationTime(DateTime.now().plusDays(14).toDate());
-        System.out.println("Token is: "+ TOKEN_SECRET);
         JWSSigner signer = new MACSigner(TOKEN_SECRET);
         SignedJWT jwt = new SignedJWT(JWT_HEADER, claim);
         jwt.sign(signer);
