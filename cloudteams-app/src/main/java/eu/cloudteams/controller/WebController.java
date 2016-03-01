baed8f2d88257b1d6b83256ccb94f3777c0cb76b
George Tsiolis <tsiolis.g@gmail.com>
2016-03-01 17:49:25 +0200

    Basic github authorization and layout loading

--- cloudteams-app/src/main/java/eu/cloudteams/controller/WebController.java
+++ cloudteams-app/src/main/java/eu/cloudteams/controller/WebController.java
@@ -43,7 +43,6 @@ public class WebController {
         return "index";
     }
 
-
     /*
      *  POST Methods 
      */
--- cloudteams-sample/src/main/resources/static/js/oauth.js
+++ cloudteams-sample/src/main/resources/static/js/oauth.js
@@ -6,35 +6,25 @@ $(document).ready(function () {
     console.log("Loaded oauth.js");
 });
 
-// Static variables
-var GITHUB_AUTHORIZE_URL= "https://github.com/login/oauth/authorize?response_type=code&client_id=413dd46351dc0c48f306&redirect_uri=http://localhost:8080/github/auth?username=" + ct_user_name + "&scope=user%20public_repo%20repo%20repo:status";
-var CLOUDTEAMS_URL = "http://localhost:8080/api/v1/auth/token?username=" + ct_user_name;
-var POPUP;
-
 /*
  *  Handlers
  */
 
 function authorizeGithubHandler() {
-
     //Make the authorize call
     $.post({
-        url: CLOUDTEAMS_URL
+        beforeSend: function (xhr){
+            if (hasAccessToken()){
+                xhr.setRequestHeader("Authorization", localStorage.auth_token);
+
+            }
+        },
+        url: "http://localhost:8080/github/repository?projectid=" + ct_project_id
     }).success(function (data, status, xhr) {
-        //console.log(data);
-        var res = JSON.parse(data);
-        if ("SUCCESS" === res.code) {
-            console.log(data);
-            localStorage.auth_token = res.token;
-        } else {
-            console.log("Could not authorize");
-        }
-//        POPUP.close();
+        $("#ct-content-github").html(data);
     });
 }
 
-function authorizeGithub(){
-    POPUP = window.open(GITHUB_AUTHORIZE_URL, "mywindow","status=0,toolbar=0,resizable=0,scrollbars=0,width=800,height=600");
-    authorizeGithubHandler();
+function hasAccessToken() {
+    return null !== localStorage.getItem("auth_token");
 }
-
