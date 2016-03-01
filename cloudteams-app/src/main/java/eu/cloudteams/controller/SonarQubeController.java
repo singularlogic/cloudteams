3cd3011f23f7e92492dd8d4857fad0936f814b1f
Christos Paraskeva <cparaskeva@ubitech.eu>
2016-03-01 13:39:34 +0200

    Fixes

--- cloudteams-app/src/main/java/eu/cloudteams/controller/SonarQubeController.java
+++ cloudteams-app/src/main/java/eu/cloudteams/controller/SonarQubeController.java
@@ -1,43 +1,41 @@
 package eu.cloudteams.controller;
 
 import eu.cloudteams.util.sonarqube.SonarIssues;
-import eu.cloudteams.util.sonarqube.SonarIssuesResponse;
-import eu.cloudteams.util.sonarqube.SonarMetricsResponse;
 import eu.cloudteams.util.sonarqube.SonarQubeService;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
  * @author Christos Paraskeva <ch.paraskeva at gmail dot com>
  */
-@RestController
-@RequestMapping("/api/v1")
-public class SonarQubeRestController {
+@Controller
+public class SonarQubeController {
 
-    private static final String SONAR_URL="https://nemo.sonarqube.org/api/";
-    private static final String PROJECT_NAME="org.apache.tika";
-    private static final String PROJECT_KEY="org.apache.tika:tika";
-    
-    
-    @RequestMapping(value = "/sonar", method = RequestMethod.GET)
-    public String egtSonar() {
-        
-            SonarQubeService sonarService = new SonarQubeService();
+    private static final String SONAR_URL = "https://nemo.sonarqube.org/api/";
+    private static final String PROJECT_NAME = "org.apache.tika";
+    private static final String PROJECT_KEY = "org.apache.tika:tika";
+
+    @ResponseBody
+    @RequestMapping(value = "/api/v1/sonar", method = RequestMethod.GET)
+    public HashMap<String, String> egtSonar() {
+
+        SonarQubeService sonarService = new SonarQubeService();
+        HashMap<String, String> projectMetrics = new HashMap<>();
 
         try {
             SonarIssues sonarIssues = sonarService.getProjectIssues(SONAR_URL, PROJECT_KEY);
-            HashMap<String, String> projectMetrics = sonarService.getProjectMetrics(SONAR_URL, PROJECT_KEY);
-            
-            
+            projectMetrics = sonarService.getProjectMetrics(SONAR_URL, PROJECT_KEY);
+
         } catch (Exception ex) {
-            Logger.getLogger(SonarQubeRestController.class.getName()).log(Level.SEVERE, null, ex);
-        }            
-        return "Sonar Controller!";
+            Logger.getLogger(SonarQubeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return projectMetrics;
 
     }
 
