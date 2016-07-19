ff8044648e6171e64fe4d7764cc56cc7c9b37aaa
Giannis Ledakis <gledakis.ubitech.eu>
2016-07-19 12:32:48 +0300

    removed username

--- bitbucket-microservice/bitbucket-util/src/main/java/eu/cloudteams/util/bitbucket/BitbucketAuthHandler.java
+++ bitbucket-microservice/bitbucket-util/src/main/java/eu/cloudteams/util/bitbucket/BitbucketAuthHandler.java
@@ -53,13 +53,13 @@ public final class BitbucketAuthHandler {
                 //Set parameters of request
                 MultiValueMap<String, String> parameteres = new LinkedMultiValueMap<>();
                 String code =jsonResponse.get("code").toString();
-                String cteamsusername =jsonResponse.get("username").toString();
+                //String cteamsusername =jsonResponse.get("username").toString();
                 //String code ="RE7PTkYmJKK6wfeSZc";
                 System.out.println("--code-->"+code );
                 parameteres.add("code", code);
                 parameteres.add("grant_type", "authorization_code");
-                parameteres.add("redirect_uri", "https://cloudteams.euprojects.net/bitbucket/api/v1/bitbucket/auth?username="+cteamsusername);
-                //parameteres.add("redirect_uri", "https://cloudteams.euprojects.net/bitbucket/api/v1/bitbucket/auth");
+                //parameteres.add("redirect_uri", "https://cloudteams.euprojects.net/bitbucket/api/v1/bitbucket/auth?username="+cteamsusername);
+                parameteres.add("redirect_uri", "https://cloudteams.euprojects.net/bitbucket/api/v1/bitbucket/auth");
                 
                           
                 HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(parameteres, headers);
